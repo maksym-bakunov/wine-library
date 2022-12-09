9edccae4e5fe87273d7b5f349233d1194a984284
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-12-09 13:37:37 +0100

    Initial commit

---
+++ src/main/java/mate/team2/winelibrary/WineLibraryApplication.java
@@ -0,0 +1,13 @@
+package mate.team2.winelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineLibraryApplication.class, args);
+    }
+
+}
